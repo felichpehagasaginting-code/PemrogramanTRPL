112d6aa467df404fb4157a3161ba218e490992a4
felich <felichpehagasaginting@gmail.com>
2026-09-21 14:00:49 +0700

    feat(loading): add root route loading skeleton

---
+++ web/app/loading.tsx
@@ -0,0 +1,5 @@
+import { LoadingScreen } from "@/components/ui/LoadingScreen";
+
+export default function RootLoading() {
+  return <LoadingScreen text="Memuat Aplikasi TRPL..." fullPage={true} />;
+}
